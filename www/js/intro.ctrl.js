fc4cf3a5355f12254c8a66ff3dcb4996a178d715
laloholguin <eholguin.-@hotmail.com>
2016-10-12 11:53:28 -0500

    initial commit

---
+++ www/js/intro.ctrl.js
@@ -0,0 +1,7 @@
+angular.module('intro.controller', [])
+.controller('IntroController', function($scope){
+
+})
+
+en el index creamos un script abajo del script src=...appJS
+<script> script="js/controllers/intro.ctrl.js"></script>
